75ab3fb0e4fa0d9c0777e0fc2ea59d66a87a4039
Edmund Mai <edmundmai@gmail.com>
2012-11-08 11:39:40 -0500

    added titles to all view pages

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -14,22 +14,14 @@ class PhotosController < ApplicationController
       @photo.save
 	end
 
-	def show
-	  @album = Album.find(params[:album_id])
-	  @photos = @album.photos
-	end
-
 	def destroy
 	  @user = User.find(params[:user_id])
 	  @album = @user.albums.find(params[:album_id])
 	  @photo = @album.photos.find(params[:id])
-	  # @photo.destroy
-	  flash[:success] = "Photo successfully deleted."
 	  respond_to do |format|
 	    if @photo.destroy
+	      flash[:success] = "Photo successfully deleted."
 	      format.js
-	      # format.json { render json: @photo, status: :created, location: @photo}
-		  # redirect_to user_album_path(@user, @album)
 		end
 	  end
 	end
